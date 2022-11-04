5202394dcd5e3228e529edb47e2fb63eea352ce8
rick2244 <84805492+rick2244@users.noreply.github.com>
2022-11-03 21:17:51 -0700

    Add files via upload

--- Lab7.java
+++ Lab7.java
@@ -75,7 +75,7 @@ public class Lab7 {
     		count++;
     		temp = temp.next;
     	}
-    	return count-1;
+    	return count;
     }
     /**
      * Calls the public version of listOps(LLN head, int[] operations) function*
@@ -96,19 +96,43 @@ public class Lab7 {
      */
     public LLN listOps(LLN head, int[] operations) {
     	LLN node = head.next;
-        for(int i = 0; i < operations.length; i++){
-        	if(operations[i] == 0) {
-        		node = opsZero(node);
-        	}
-        	else if(operations[i] == 1) {
-        		node = opsOne(node);
-        	}
-        	else if(operations[i] == 2) {
-        		node = opsTwo(node);
-        	}
-        	
-        }
-        return node;
+    	//If conditional handles if the operation is {2} and it prints out the right
+    	//node if that is it
+    	if(operations.length == 1 && operations[0] == 2) {
+    		node = opsZero(node);
+    		//For loop walks the linked lists to the start of the second half of the linked list 
+			//This conditional handles when the linked list is odd
+    		if(size(node)%2 == 1) {
+				for(int i = 0; i < size(head)/2-1;i++) {
+	    			node = node.next;
+	    		}
+			}//This conditional handles when the linked list is even
+			else if(size(node)%2 == 0) {
+				for(int i = 0; i < size(head)/2;i++) {
+	    			node = node.next;
+	    		}
+			}
+			//returns the second half of the linked list
+    		return node;
+    	}
+    	//This else conditional handles every other situation  in which the operation isn't {2}
+    	else {
+    		for(int i = 0; i < operations.length; i++){
+            	if(operations[i] == 0) {
+            		node = opsZero(node);
+            	}
+            	else if(operations[i] == 1) {
+            		node = opsOne(node);
+            	}
+            	else if(operations[i] == 2) {
+            		node = opsTwo(node);
+            	}
+            	
+            }
+    		//returns the full node starting with 1
+            return node;
+    	}
+        
     }
 
     /**
@@ -121,6 +145,9 @@ public class Lab7 {
     	int length = size(head);
     	LLN temp = head;
     	LLN second_half = null;
+    	//These two if conditionals handle whether the linked list is even or odd and finds the right
+    	//middle node based on those conditionals
+    	//First finds if the linked list is odd
     	if(length%2 == 0) {
     		for(int i = 0; i < length; i++) {
     			if(i == length/2) {
@@ -128,18 +155,19 @@ public class Lab7 {
     			}
     			temp = temp.next;
     		}
-       	}
+       	}//Second conditional handles if the linked list is even
     	else if(length%2 == 1) {
     		for(int i = 0; i < length; i++) {
-    			if(i == length/2+1) {
+    			if(i == length/2) {
     				second_half = temp;
     			}
     			temp = temp.next;
     		}
     	}
-        System.out.println(second_half.val);
+    	//prints out the middle node
+        System.out.println("\"" + second_half.val + "\"");
         //printTrue(second_half);
-        return head; //TODO complete this function, remove this line when you start
+        return head;
     }
 
     /**
@@ -149,18 +177,20 @@ public class Lab7 {
      * @return
      */
     public LLN opsOne(LLN head) {
-        //TODO complete this function, remove this line when you start
     	LLN prev, curr, next;
     	prev = null;
     	curr = head;
     	next = null;
+    	//While loop adds the values of head to prev in reverse order
     	while(curr != null) {
     		next = curr.next;
     		curr.next = prev;
     		prev = curr;
     		curr = next;
     	}
+    	//sets head to prev
     	head = prev;
+    	//returns reversed oder of linked list
     	return head;
     }
 
@@ -171,7 +201,6 @@ public class Lab7 {
      * @return
      */
     public LLN opsTwo(LLN head) {
-        //TODO complete this function, remove this line when you start
         LLN first_half, curr,next, prev;
         first_half = null;
         curr = head;
@@ -179,36 +208,25 @@ public class Lab7 {
         prev = null;
         
         int len = size(head);
-        
-        if(len%2 == 0) {
-        	for(int i = 0; i < len/2; i++) {
-        		first_half = curr;
-        		curr = curr.next;
-        	}
-        	
-        	while(curr != null) {
-        		next = curr.next;
-        		curr.next = prev;
-        		prev = curr;
-        		curr = next;
-        	}
-        }
-        else if(len%2 == 1) {
-        	for(int i = 0; i < len/2+1; i++) {
-        		first_half = curr;
-        		curr = curr.next;
-        	}
-        	while(curr != null) {
-        		next = curr.next;
-        		curr.next = prev;
-        		prev = curr;
-        		curr = next;
-        	}
-        }
+        //The first for loop handles the finding the first half of the linked list
+    	for(int i = 0; i < len/2; i++) {
+    		first_half = curr;
+    		curr = curr.next;
+    	}
+    	//This for loop reverse the second half of the linked list
+    	while(curr != null) {
+    		next = curr.next;
+    		curr.next = prev;
+    		prev = curr;
+    		curr = next;
+    	}
+    	//Links the two linked list halfs together
         first_half.next = prev;
         return head;
 
     }
+    //This is my own print statement that prints out the values of the linked list in
+    //the desired format of the lab requirements
     public void printTrue(LLN head) {
     	LLN walk = head;
     	System.out.print("[");
@@ -227,21 +245,22 @@ public class Lab7 {
 
     public static void main(String[] args) {
         Lab7 lln = new Lab7();
-        lln.add(11);
-        lln.add(2);
-        lln.add(-4);
-        lln.add(5);
         lln.add(1);
-        //lln.add(6);
-        lln.print();// 0->1->2->3->4->5->NULL
+        lln.add(2);
+        lln.add(3);
+        lln.add(4);
+        lln.add(5);
+        lln.add(6);
+        //lln.add(7);
+        //lln.add(8);
+        //lln.print();// 0->1->2->3->4->5->NULL
 
-        //Reverse whole list
+        
         LLN ret;
-        ret = lln.listOps(new int[] {2,1,2,1});
+        ret = lln.listOps(new int[] {2,1,2,1,2});
         //lln.print(ret);//5->4->3->2->1->0->NULL
         lln.printTrue(ret);
-        //Test your code here
-        //......
+        
 
     }
 }
